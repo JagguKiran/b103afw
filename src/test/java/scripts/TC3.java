a97c18b50595b1b9c1e3a0baa3cdb63bb7043174
kiran <kiran.kiranjmps@gmail.com>
2024-07-09 19:43:34 +0530

    New Class is Created

---
+++ src/test/java/scripts/TC3.java
@@ -0,0 +1,12 @@
+package scripts;
+
+import org.testng.annotations.Test;
+
+import generic.BaseTest;
+
+public class TC3 extends BaseTest{
+	@Test
+	public void test() {
+		test.info("New Test Case is Added");
+	}
+}
